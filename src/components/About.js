892c1925ef474136b41bd88496cb8ffbcfb06ad5
Kim Huiyeon <gnldus59112@gmail.com>
2019-03-22 16:01:18 +0800

    Updated About

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import Fade from 'react-reveal/Fade'
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+import Img from 'gatsby-image'
 
 class About extends React.Component {
     render() {
         return (
             <section style={{width:`calc(100vw - (100vw - 100%))`, height:`100vh`}}>
                 <h2 style={{position:`relative`, top:40, textAlign:`center`, fontWeight:300, color:`#fa7268`, fontSize:40}}>About Me</h2>
-                <div className="container" style={{display:`grid`, gridTemplateColumns:`1fr 1fr`}}>
-                    <Fade left>
-                        <h1>Hello World</h1>
-                    </Fade>
-                    <Fade right>
-                        <h1>Bye World</h1>
-                    </Fade>
+                <div className="container" style={{marginTop:130}}>
+                    <div className="row">
+                        <Fade left>
+                            <div className="col s12 m12 l6">
+                                <Img fluid={this.props.fluid} style={{borderRadius:`50%`, boxShadow:`0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)`, marginBottom:30, maxWidth:`400px`}} alt="Hannah Lee" title="Hyeonjeong Lee"/>
+                            </div>
+                        </Fade>
+                        <Fade right>
+                        <div className="col s12 m12 l6">
+                            <h3 style={{fontFamily:`-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"`, color:`black`, fontWeight:`bold`, margin:0,padding:0, marginBottom:`1.45rem`,textRendering:`optimizeLegibility`, lineHeight:1.1, fontSize:`1.38316rem`}}>Hi, I'm Huiyeon Kim. I am a Developer, Tech Enthusiast and a Writer.</h3>
+                            <p style={{color:`black`, fontFamily:`-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"`}}>I am currently a Penultimate Student studying in Singapore Management University, B.Sc. Information Systems. 
+                            Want to know more about what Im learning now? Visit <span style={{color:`#2b33b2`, cursor:`pointer`}} onClick={() => window.open("https://learnwars.com", "__blank")}><b>LearnWars</b></span> and read about it!
+                            </p>
+                        </div>
+                        </Fade>
+                    </div>
                 </div>
             </section>
         )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,39 @@ import Landing from "../components/Landing"
 import Work from '../components/Work'
 import SEO from "../components/seo"
 import About from "../components/About"
+import {graphql} from 'gatsby'
 
-const IndexPage = () => (
-  <div>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <Header />
-    <Landing />
-    <Work />
-    <About />
-  </div>
-)
+const IndexPage = ({data}) => {
+    const hannahPic = data.allFile.edges.filter(edge => edge.node.childImageSharp.fluid.originalName === "hannahround.png");
+    console.log(hannahPic)
+    return (
+        <div>
+            <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+            <Header />
+            <Landing />
+            <Work />
+            <About fluid={hannahPic[0].node.childImageSharp.fluid}/>
+        </div>
+    )
+}
 
 export default IndexPage
+
+export const query = graphql`
+query {
+    allFile {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 2600) {
+              originalName
+              ...GatsbyImageSharpFluid
+            }
+          }
+          publicURL
+        }
+      }
+    }
+  }
+  
+`
